a7230cea89fc0886f44544dcba892532711d8bd7
sugasaki <sugasaki@gmail.com>
2021-01-13 13:05:59 +0900

    コメント修正

--- es-kotlin-console/src/main/kotlin/02-main.kt
+++ es-kotlin-console/src/main/kotlin/02-main.kt
@@ -3,14 +3,14 @@ import org.elasticsearch.client.indexRepository
 import thing.Thing
 import thing.ThingSearch
 
-fun main(args: Array<String>) {
+fun main() {
     // connects to elastic cloud
     val esClient = create(host = "localhost", port = 9200)
 
     val thingRepository = esClient.indexRepository<Thing>("things")
     val recipeSearch = ThingSearch(thingRepository)
 
-    // indexn作成
+    // indexを作成
     recipeSearch.deleteIndex()
     recipeSearch.createNewIndex()
     recipeSearch.consolePrintMappings() // debug print
