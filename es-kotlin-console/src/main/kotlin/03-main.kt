ca251ccb45143606201218f145ca27480f779d20
sugasaki <sugasaki@gmail.com>
2021-01-13 13:13:09 +0900

    3. CRUD 操作を追加

--- es-kotlin-console/src/main/kotlin/03-main.kt
+++ es-kotlin-console/src/main/kotlin/03-main.kt
@@ -16,7 +16,7 @@ fun main() {
     recipeSearch.createNewIndex()
 
     // 1. ドキュメント追加
-    var id = "first"
+    val id = "first"
     println("Object does not exist: ${thingRepository.get(id)}")
 
     // so lets store something
@@ -24,13 +24,14 @@ fun main() {
     println("Now we get back our object: ${thingRepository.get(id)}")
 
     // 2. ドキュメント上書き
-    id = "first"
+    // 2.1. ドキュメント上書き。こちらは失敗する
     try {
         thingRepository.index(id, Thing("A thing", 40))
     } catch (e: ElasticsearchStatusException) {
         println("we already had one of those and es returned ${e.status().status}")
     }
 
+    // 2.2. create = falseに設定してのドキュメント上書き。こちらは成功する
     // this how you do upserts
     thingRepository.index(id, Thing("Another thing", 666), create = false)
     println("It was changed: ${thingRepository.get(id)}")
